73cab56eb4470b277296426f537b62f353efeaf7
crazy-cat-crypto <226846482+crazy-cat-crypto@users.noreply.github.com>
2026-02-23 07:28:57 +0545

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app=Flask(__name__)
 def home():
     if request.method == "POST":
         email_data = request.get_json()
-        email_form= email_data.get("from")
+        email_from= email_data.get("from")
         email_subject= email_data.get("subject")
         email_body= email_data.get("body")
         print(f"Received email from: {email_from}, with subject: {email_subject}, and body: {email_body}")
