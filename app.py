bd10a15a154de076e209b5510721f0bdcf2c4964
crazy-cat-crypto <226846482+crazy-cat-crypto@users.noreply.github.com>
2026-02-23 00:30:09 +0000

    made email receiveable

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask,request,jsonify
 app=Flask(__name__)
 
 
-@app.route("/")
-def receive_email():
+@app.route("/", methods=["POST","GET"])
+def home():
+    if request.method == "POST":
+        email_data = request.get_json()
+        email_form= email_data.get("form")
+        email_subject= email_data.get("subject")
+        email_body= email_data.get("body")
+        print(f"Received email from: {email_form}, with subject: {email_subject}, and body: {email_body}")
+        return jsonify({"message": "Email received successfully!"}), 200
     return "Hello! This is the email receiver endpoint."
